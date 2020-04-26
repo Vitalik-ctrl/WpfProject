94c88f2d60dac6a7eb6046c7b3c30ba08a25b8d1
Vitalik Varhanik <vitalikvarhanik@gmail.com>
2020-04-26 10:37:51 +0300

    Change#4

--- WpfProject_Part1/RegScreen.xaml.cs
+++ WpfProject_Part1/RegScreen.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -23,9 +24,33 @@ namespace WpfProject_Part1
             InitializeComponent();
         }
 
-        private void btnReg_Click(object sender, RoutedEventArgs e)
-        {
-            /// https://it-black.ru/rabota-s-bazoj-dannyh-v-ci_sharp/ --- полезная ссылка
+        private void btn1_Click(object sender, RoutedEventArgs e)
+       {
+            using (SqlConnection connection = new SqlConnection(@"Data Source =HOME-PC\SQLEXPRESS; Initial Catalog=WPFprojectDB; Integrated Security=True;"))
+            {
+                 String login = txtUsername.Text;
+                 String password = txtPassword.Password;
+                 String lastName = txtLastName.Text;
+                 String firstName = txtFirstName.Text;
+                 String email = txtEmail.Text;
+
+
+
+                using (SqlCommand command = new SqlCommand("INSERT INTO tdb(UserID, UserName, Password, Lastname, Firstname, Email) VALUES(@UserID, @UserName, @Password, @Lastname, @Firstname, @Email)", connection))
+                {
+                    command.Parameters.AddWithValue("@UserID", "1" );
+                    command.Parameters.AddWithValue("@UserName", login);
+                    command.Parameters.AddWithValue("@Password", password);
+                    command.Parameters.AddWithValue("@Lastname", lastName);
+                    command.Parameters.AddWithValue("@Firstname", firstName);
+                    command.Parameters.AddWithValue("@Email", email);
+
+
+                    command.Connection.Open();
+                    command.ExecuteNonQuery();
+                }
+            }
         }
     }
-}
+    }
+
